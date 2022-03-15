4b700dccaa0640f0f82bc2a51e1f72a1145a2597
Santiago Julian <sjulian983@gmail.com>
2022-03-15 18:18:50 +0100

    labels inputs and button created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">frontend</div>;
+  return (
+    <div className="App">
+      <h1>CRUD Operations</h1>
+
+      <label>Food Name:</label>
+      <input type="text" />
+
+      <label>Days since you ate it:</label>
+      <input type="number" />
+
+      <button>Add to list</button>
+    </div>
+  );
 }
 
 export default App;
